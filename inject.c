66482c54970f7f03c69accc2d4afc3cda8deb604
Willy Tarreau <w@1wt.eu>
2009-10-21 10:49:52 +0200

    [RELEASE] version 3

--- inject.c
+++ inject.c
@@ -15,12 +15,11 @@
 #include <libNetTools.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <signal.h>
 
+#define METH_NONE	0
 #define METH_GET	1
 #define METH_POST	2
-
-#define NBPAGES		4
-//#define NBPAGES		2
 
 #define STATUS_START	1
 #define STATUS_RUN	2
@@ -30,6 +29,8 @@
 #define BUFSIZE		1024
 #define MAXSOCK		1000
 
+#define Log(a, b...)	;
+
 struct pageobj {
     struct sockaddr_in addr;
     int fd;
@@ -38,6 +39,7 @@ struct pageobj {
     char meth;
     char *uri;
     char *vars;
+    char *host;
     struct pageobj *next;
     struct page *page;
 };
@@ -65,20 +67,38 @@ struct client {
     struct client *next;
 };
 
+struct scnobj {
+    char meth;
+    char *host;
+    char *uri;
+    char *vars;
+    struct scnobj *next;
+};
+
+struct scnpage {
+    int thinktime;
+    char *name;
+    struct scnobj *objects;
+    struct scnpage *next;
+} *firstscnpage = NULL;
+
+struct scnpage *curscnpage = NULL;
+struct scnobj  *curscnobj = NULL;
+char curscnhost[64]="host not set !";
+
 struct client *clients = NULL;
-int nbclients=100;
-//char *appaddr ="192.32.83.175:7999";
-char *appaddr ="192.32.83.175:7979";
-//char *appaddr ="192.32.83.176:7979";
-char *imgaddr ="192.32.83.175:80";
+unsigned long int nbclients=100;
+unsigned long int nbiterations, iterations=0;
 int injectdelay = 500;
-int thinktime = 20000;
-static struct timeval now;
+static struct timeval now={0,0};
 int nbconn=0;
 int clientid=0;
 int nbactcli=0;
 long int totalread=0;
 char trash[16384];
+char *scnfile = NULL;
+static struct timeval starttime;
+static int needschedule = 0;
 
 /* crée un objet pour une page :
    methode = { METH_GET | METH_POST } 
@@ -96,6 +116,7 @@ struct pageobj *newobj(char methode, char *addr, char *uri, char *vars, struct p
     obj->fd	= -1;  /* fd non ouvert */
     obj->meth	= methode;
     obj->uri	= strdup(uri);
+    obj->host	= strdup(addr);
     if (vars != NULL)
 	obj->vars	= strdup(vars);
     obj->next	= NULL;
@@ -109,44 +130,29 @@ struct pageobj *newobj(char methode, char *addr, char *uri, char *vars, struct p
     return obj;
 }
 
-/* crée une page pour un client existant. Le username et password du client doivent exister */
-struct page *newpage(int position, int time, struct client *client) {
+/* crée une page pour un client existant. Les username et password du client doivent exister */
+struct page *newpage(struct scnpage *scn, struct client *client) {
     struct page *page;
     struct pageobj **obj;
+    struct scnobj *scnobj;
     char variables[256];
 
     page=(struct page *)calloc(1, sizeof(struct page));
     page->client = client;
-    page->thinktime = time;
+    page->thinktime = scn->thinktime;
+
     obj = &(page->objects);
-    
-    switch (position) {
-    case 0 : /* page d'authentification */
-	*obj = newobj(METH_GET, appaddr, "/accesi/ia10.html", NULL, page);
+
+    for (scnobj = scn->objects; scnobj; scnobj = scnobj->next) {
+	if ((scnobj->vars != NULL) && (client != NULL) &&
+	    (client->username != NULL) && (client->password != NULL)) {
+	    sprintf(variables, scnobj->vars, client->username, client->password);
+	    *obj = newobj(scnobj->meth, scnobj->host, scnobj->uri, variables, page);
+	}
+	else 
+	    *obj = newobj(scnobj->meth, scnobj->host, scnobj->uri, NULL, page);
+
 	obj=&((*obj)->next);
-	break;
-    case 1 : /* validation de la page d'authentification -> liste des comptes */
-	if ((client != NULL) && (client->username != NULL) && (client->password != NULL))
-	    sprintf(variables, "codcli1=%s&codsec=%s", client->username, client->password);
-	else
-	    *variables=0;
-	*obj = newobj(METH_POST, appaddr, "/accesi/ia10.html", variables, page);
-	obj=&((*obj)->next);
-	*obj = newobj(METH_GET, appaddr, "/menu/menu.html", NULL, page);
-	obj=&((*obj)->next);
-	*obj = newobj(METH_GET, appaddr, "/consultation/consu10.html", NULL, page);
-	obj=&((*obj)->next);
-	break;
-    case 2 : /* détail d'un compte */
-	*obj = newobj(METH_GET, appaddr, "/consultation/cav20.html", NULL, page);
-	obj=&((*obj)->next);
-	break;
-    case 3 : /* quitte */
-	*obj = newobj(METH_GET, appaddr, "/accesi/byebye.html", NULL, page);
-	obj=&((*obj)->next);
-	break;
-    default :
-	break;
     }
     return page;
 }
@@ -154,7 +160,7 @@ struct page *newpage(int position, int time, struct client *client) {
 void newclient(char *username, char *password) {
     struct client *newclient;
     struct page **page;
-    int i;
+    struct scnpage *scn;
 
     newclient=(struct client*)calloc(1, sizeof(struct client));
     newclient->cookie=NULL;
@@ -164,8 +170,8 @@ void newclient(char *username, char *password) {
 	newclient->password=strdup(password);
 
     page=&(newclient->pages);
-    for (i=0; i<NBPAGES; i++) {
-	*page=newpage(i, thinktime, newclient);
+    for (scn = firstscnpage; scn != NULL; scn = scn->next) {
+	*page=newpage(scn, newclient);
 	page=&((*page)->next);
     }
 
@@ -219,6 +225,128 @@ void destroyclient(struct client *client) {
     free(client);
 }
 
+/* creates a new scenario page, set its name and thinktime */
+struct scnpage *newscnpage(char *name, int thinktime) {
+    struct scnpage *page = (struct scnpage *)calloc(1, sizeof(struct scnpage));
+
+    if (curscnpage != NULL) {
+	curscnpage->next = page;
+    }
+    else {
+	firstscnpage = page;
+    }
+    page->thinktime = thinktime;
+    page->name = strdup(name);
+
+    curscnobj = NULL;
+
+    Log(LOG_LDEBUG, "[inject] - New page : %s %d\n", name, thinktime);
+    return curscnpage = page;
+}
+
+/* adds a new object to the current page. aborts if no current page set */
+struct scnobj *newscnobj(int meth, char *host, char *uri, char *vars) {
+    struct scnobj *obj;
+
+    if (curscnpage == NULL)
+	Abort("Scenario must define PAGE before OBJECT.\n");
+
+    obj = (struct scnobj *)calloc(1, sizeof(struct scnobj));
+
+    obj->meth = meth;
+    obj->host = strdup(host);
+    obj->uri = strdup(uri);
+    obj->vars = (vars == NULL) ? NULL : strdup(vars);
+
+    if (curscnobj == NULL)
+	curscnpage->objects = obj;
+    else
+	curscnobj->next = obj;
+
+    Log(LOG_LDEBUG, "[inject] --- New object : meth=<%d> host=<%s> uri=<%s> vars=<%p>\n", meth, host, uri, vars);
+
+    return curscnobj = obj;
+}
+
+/* returns 0 if OK, -1 if error */
+int parsescnline(char *line) {
+    char *cmd;
+    char *pagename;
+    long int time;
+    char *srv;
+    char *vars;
+    int meth;
+
+    char *args[10];
+    int arg;
+
+    /* skips leading spaces */
+    while (isspace(*line))
+	line++;
+
+    /* cleans up line contents */
+    cmd = line;
+    while (*cmd) {
+	if (*cmd == '#' || *cmd == ';' || *cmd == '\n' || *cmd == '\r')
+	    *cmd = 0; /* end of string, end of loop */
+	else
+	    cmd++;
+    }
+
+    if (*line == 0)
+	return 0;
+
+    /* fills args with the line contents */
+    for (arg=0; arg<9; arg++) {
+	args[arg] = line;
+	while (*line && !isspace(*line)) line++;
+	if (*line) {
+	    *(line++) = 0;
+	    while (isspace(*line))
+		line++;
+	}
+    }
+
+    if (**args == 'H' || **args == 'h') {  /* host */
+	if (isalnum(*args[1])) {
+	    strncpy0(curscnhost, args[1], sizeof(curscnhost));
+	    Log(LOG_LDEBUG,"[inject] <parsescnline> : host is now %s\n",curscnhost);
+	}
+	return 0;
+    }
+    
+    if (**args == 'N' || **args == 'n') { /* new page */
+	pagename = args[1];
+	time = atol(args[2]);
+	newscnpage(pagename, time);
+	return 0;
+    }
+
+    meth = METH_NONE;
+    if (**args == 'G' || **args == 'g')  /* new object : GET */
+	meth = METH_GET;
+    else if (**args == 'P' || **args == 'p')  /* new object : POST */
+	meth = METH_POST;
+
+    if (meth != METH_NONE) {
+	if (*args[1] == '.') /* stay on current host */
+	    srv = curscnhost;
+	else
+	    srv = args[1];
+
+	if (*args[3])
+	    vars = args[3];
+	else
+	    vars = NULL;
+	newscnobj(meth, srv, args[2], vars);
+	return 0;
+    }
+
+    Log(LOG_LERR,"[inject] <parsescnline> : args=<%s> <%s> <%s> <%s> ...\n",args[0], args[1], args[2], args[3]);
+
+    return -1; /* unknown sequence */
+}
+
 
 /****************************************/
 
@@ -228,6 +356,10 @@ void Event(int fd, int flags, struct pageobj *obj) {
     char *r = req;
 
     Log(LOG_LFUNC,"[Event].\n");
+
+    /* small optimization for the scheduler */
+    needschedule = 1;
+
     if (flags & EV_WRITE) {
 	int data, ldata;
 
@@ -261,13 +393,14 @@ void Event(int fd, int flags, struct pageobj *obj) {
 	    else
 		r+=sprintf(r,"\r\n");
 	}
-	else /* meth = METH_GEST */ {
+	else /* meth = METH_GET */ {
 	    r+=sprintf(r, "GET %s", obj->uri);
 	    if (obj->vars)
 		r+=sprintf(r,"?%s", obj->vars);
 	    r+=sprintf(r," HTTP/1.0\r\n");
 	    if (obj->page->client->cookie)
 		r+=sprintf(r, "Cookie: %s\r\n", obj->page->client->cookie);
+	    r+=sprintf(r, "Host: %s\r\n", obj->host);
 	    r+=sprintf(r,"\r\n");
 	}
 	/* la requete est maintenant prete */
@@ -301,6 +434,10 @@ void Event(int fd, int flags, struct pageobj *obj) {
 	Log(LOG_LDEBUG,"[Event] socket <%d> du client <%s> : read() = %d.\n",
 	    fd, obj->page->client->username, ret);
 	if (ret>0) {
+
+	    if ((starttime.tv_sec | starttime.tv_usec) == 0)  /* la premiere fois, on démarre le chrono */
+		tv_now(&starttime);
+
 	    obj->page->dataread+=ret;
 	    obj->page->client->dataread+=ret;
 	}
@@ -402,27 +539,29 @@ void Event(int fd, int flags, struct pageobj *obj) {
     }
 }
 
-/* ce scheduler s'occupe de gérer les clients (think time, ...)
-   et d'injecter des clients tant qu'il n'y en a pas assez, mais jamais
+/* ce scheduler s'occupe d'injecter des clients tant qu'il n'y en a pas assez, mais jamais
    plus d'un tous les <injectdelay> ms
 */
 int injecteur(void *arg) {
     static struct timeval next, now;
-    struct client *cli;
     unsigned int delay;
     char uname[32];
-    int sock;
 
     Log(LOG_LFUNC,"[injecteur].\n");
     tv_now(&now);
+
     if (nbconn < MAXSOCK) {
-	if ((nbactcli < nbclients) && (tv_cmp(&next, &now) <= 0)) {  /* on doit créer un nouveau client */
+	if ((nbactcli < nbclients)  && (iterations < nbiterations)
+	    && (tv_cmp(&next, &now) <= 0)) {  /* on doit créer un nouveau client */
 	    sprintf(uname, "%d", clientid++);
+	    /* on entre dans le scheduler directement à partir de ce client */
 	    newclient(uname, uname);
 	    nbactcli++;
 	    Log(LOG_LAPP, "Création du client <%s> : <%d> clients actifs.\n",uname,nbactcli);
 	    delay=injectdelay;
 	    tv_delayfrom(&next, &now, injectdelay);
+	    iterations++;
+	    needschedule = 1;
 	}
 	else
 	    delay=tv_delta(&next, &now);
@@ -430,7 +569,30 @@ int injecteur(void *arg) {
     else
 	delay = 1000;  /* 1 seconde si plus de sockets */
 
+    return delay;
+}
+
+
+/* ce scheduler s'occupe de gérer les clients (think time, ...)
+*/
+int scheduler(void *arg) {
+    static struct timeval next, now;
+    struct client *cli;
+    unsigned int delay;
+    char uname[32];
+    int sock;
+
+    Log(LOG_LFUNC,"[scheduler].\n");
+    tv_now(&now);
+
     cli = clients;
+    delay=tv_remain(&now, &next);
+
+    if (!needschedule && delay>0)
+	return delay;
+
+    needschedule = 0;
+
     while (cli) {
 	struct page *page;
 	struct pageobj *obj;
@@ -445,12 +607,14 @@ int injecteur(void *arg) {
 		if (cli->current == NULL) {  /* fin du scénario pour ce client */
 		    struct client *newcli = cli->next;
 		    
-		    Log(LOG_LAPP, "Terminaison du client <%s> : <%d> octets lus, <%ld> au total.\n",
-			cli->username, cli->dataread, totalread);
+		    Log(LOG_LAPP, "Terminaison du client <%s> : <%d> octets lus, <%ld> au total -> moy = %ld kB/s.\n",
+			cli->username, cli->dataread, totalread,
+			totalread / (tv_delta(&now, &starttime)?:1));
 		    totalread += cli->dataread;
 		    destroyclient(cli);
 		    cli=newcli;
 		    nbactcli--;
+
 		    continue;
 		}
 		else {
@@ -459,7 +623,7 @@ int injecteur(void *arg) {
 	    }
 	    else {
 		int del2;
-		del2 = tv_delta(&now, &cli->nextevent);
+		del2 = tv_remain(&now, &cli->nextevent);
 		if (del2 < delay)
 		    delay=del2;
 	    }
@@ -473,7 +637,7 @@ int injecteur(void *arg) {
 	    cli=newcli;
 	    nbactcli--;
 	    Reschedule();  /* on peut avoir à relancer des clients */
-	    continue;
+	    goto nextclient;
 	}
 
 	if (cli->status == STATUS_START) {  /* envoyer une nouvelle page */
@@ -486,13 +650,26 @@ int injecteur(void *arg) {
 		    Log(LOG_LDEBUG, "connexion de l'objet <%s> de la page <%p> pour le client <%s>.\n",
 			obj->uri, page, cli->username);
 		    if ((obj->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
-			Log(LOG_LERR, "impossible ce créer une socket\n");
-			break;
+	    		struct client *newcli = cli->next;
+
+			Log(LOG_LERR, "impossible de créer une socket\n");
+		        destroyclient(cli);
+		        cli=newcli;
+		        nbactcli--;
+		        Reschedule();  /* on peut avoir à relancer des clients */
+		        goto nextclient;
 		    }
 		    if (fcntl(obj->fd, F_SETFL, O_NONBLOCK)==-1) {
 			Log(LOG_LERR,"impossible de mettre la socket en O_NONBLOCK\n");
 		    } else if ((connect(obj->fd, &obj->addr, sizeof(obj->addr)) == -1) && (errno != EINPROGRESS)) {
+	    		struct client *newcli = cli->next;
+
 			Log(LOG_LERR,"impossible de faire le connect() pour %d\n", obj->fd);
+		        destroyclient(cli);
+		        cli=newcli;
+		        nbactcli--;
+		        Reschedule();  /* on peut avoir à relancer des clients */
+		        goto nextclient;
 		    }
 		    else {
 			nbconn++;
@@ -508,23 +685,68 @@ int injecteur(void *arg) {
 	}
 
 	cli = cli->next;
+nextclient:
+    }
+
+    if ((nbactcli == 0) && (iterations == nbiterations)) {
+	Log(LOG_LPERF, "Fin: %ld clients ont chargé %ld octets en %ld ms : %ld kB/s.\n",
+	    iterations, totalread, tv_delta(&now, &starttime), totalread/(tv_delta(&now, &starttime)?:1));
+	printf("Fin: %ld clients ont chargé %ld octets en %ld ms : %ld kB/s.\n",
+	    iterations, totalread, tv_delta(&now, &starttime), totalread/(tv_delta(&now, &starttime)?:1));
+	exit(0);
     }
 
     return delay;
 }
 
 
+/* returns 0 if OK, -1 if error */
+int readscnfile(char *file) {
+    FILE *f;
+
+    if ((f=fopen(file,"r")) == NULL)
+	return -1;
+
+    while (fgets(trash, sizeof(trash), f) != NULL) {
+	if (parsescnline(trash) < 0)
+	    return -1;
+    }
+    fclose(f);
+    return 0;
+}
+
+
+void stopper(int sig) {
+    static struct timeval now;
+    tv_now(&now);
+    printf("Arret: %ld clients ont chargé %ld octets en %ld ms : %ld kB/s.\n",
+	   iterations, totalread, tv_delta(&now, &starttime), totalread/(tv_delta(&now, &starttime)?:1));
+    exit(0);
+}
+
+
 int main(int argc, char **argv) {
 
-    if (argc != 4) {
-	fprintf(stderr,"Syntaxe : inject <nbusers> <injectdelay> <think time>\n");
+    if (argc != 5) {
+	fprintf(stderr,"Syntaxe : inject <nbusers> <nbiterations> <injectdelay> <scnfile>\n");
 	exit(1);
     }
     nbclients = atol(argv[1]);
-    injectdelay = atol(argv[2]);
-    thinktime = atol(argv[3]);
-    LogOpen("/tmp/injecteur.log", LOG_LPERF | LOG_LWARN | LOG_LERR | LOG_LAPP);
+    nbiterations = atol(argv[2]) * nbclients;
+    injectdelay = atol(argv[3]);
+    scnfile = argv[4];
+    tv_now(&starttime);  /* just in case it's stopped too early */
+    signal(SIGINT, &stopper);
+
+    //    LogOpen("/tmp/injecteur.log", LOG_LERR | LOG_LPERF | LOG_LWARN | LOG_LAPP);
+
+    if (readscnfile(scnfile) < 0) {
+	Log(LOG_LFATAL, "[inject] Error reading scn file : %s\n", scnfile);
+	exit(1);
+    }
+
     RegisterScheduler(&injecteur, NULL);
+    RegisterScheduler(&scheduler, NULL);
     SelectRun();
     return 0;
 }
